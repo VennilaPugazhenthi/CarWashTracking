ddf8a71575d4501004cf9a2de1824a6ffe90f447
Vennila Pugazhenthi <vennilapugazhenthi@gmail.com>
2022-03-04 20:04:33 -0500

    Initial commit

---
+++ src/main/java/com/vanavil/CarWashTracking/CarWashTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.vanavil.CarWashTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWashTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanavil/CarWashTracking/CarWashTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanavil.CarWashTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
